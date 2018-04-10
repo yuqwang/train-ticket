88f8406fafd5ba5a014f395e4fadf687df8468d3
lab401\lwh <2391289924@qq.com>
2018-04-10 17:57:48 +0800

    Add the prefix in the ticket office service

--- ts-ticket-office-service/bin/router.js
+++ ts-ticket-office-service/bin/router.js
@@ -19,7 +19,7 @@ router.get('/welcome', function(req, res, next) {
 //     });
 // });
 
-router.get('/getRegionList', function(req, res, next) {
+router.get('/office/getRegionList', function(req, res, next) {
     /*读取region.json*/
     fs.readFile(path.join(__dirname, "./region.json"), 'utf8', function (err, data) {
         data = JSON.parse( data );
@@ -28,7 +28,7 @@ router.get('/getRegionList', function(req, res, next) {
     });
 });
 
-router.get('/getAll', function(req, res, next) {
+router.get('/office/getAll', function(req, res, next) {
     db.getAll(function(result){
         res.set({'Content-Type':'text/json','Encodeing':'utf8'});
         console.log("getAll=" + JSON.stringify(result));
@@ -36,7 +36,7 @@ router.get('/getAll', function(req, res, next) {
     });
 });
 
-router.post('/getSpecificOffices', function(req, res, next) {
+router.post('/office/getSpecificOffices', function(req, res, next) {
     db.getSpecificOffices(req.body.province, req.body.city, req.body.region, function(result){
         res.set({'Content-Type':'text/json','Encodeing':'utf8'});
         console.log("getSpecificOffices=" + JSON.stringify(result));
@@ -44,7 +44,7 @@ router.post('/getSpecificOffices', function(req, res, next) {
     });
 });
 
-router.post('/addOffice', function(req, res, next) {
+router.post('/office/addOffice', function(req, res, next) {
     db.addOffice(req.body.province, req.body.city, req.body.region, req.body.office, function(result){
         res.set({'Content-Type':'text/json','Encodeing':'utf8'});
         console.log("addOffice=" + JSON.stringify(result));
@@ -53,7 +53,7 @@ router.post('/addOffice', function(req, res, next) {
 });
 
 //此处注意，同一个区默认不能有两个名字相同的代售点
-router.post('/deleteOffice', function(req, res, next) {
+router.post('/office/deleteOffice', function(req, res, next) {
     db.deleteOffice(req.body.province, req.body.city, req.body.region, req.body.officeName, function(result){
         res.set({'Content-Type':'text/json','Encodeing':'utf8'});
         console.log("deleteOffice=" + JSON.stringify(result));
@@ -62,7 +62,7 @@ router.post('/deleteOffice', function(req, res, next) {
 });
 
 //此处注意，同一个区默认不能有两个名字相同的代售点
-router.post('/updateOffice', function(req, res, next) {
+router.post('/office/updateOffice', function(req, res, next) {
     db.updateOffice(req.body.province, req.body.city, req.body.region, req.body.oldOfficeName, req.body.newOffice, function(result){
         res.set({'Content-Type':'text/json','Encodeing':'utf8'});
         console.log("updateOffice=" + JSON.stringify(result));
