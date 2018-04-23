a6bb1f7817e581e900458c2b53c8944309f35593
Ji Chao <chaojifudan@outlook.com>
2018-04-23 18:35:04 +0800

    modify testcase

---
+++ ts-ui-test/src/test/java/CancelOrderResult.java
@@ -0,0 +1,26 @@
+public class CancelOrderResult {
+
+    private boolean status;
+
+    private String message;
+
+    public CancelOrderResult() {
+        //Default Constructor
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
