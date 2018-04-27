026fffa9c8cab7eea278f8e574a953a07ff5398e
Ji Chao <chaojifudan@outlook.com>
2018-04-27 19:35:14 +0800

    Modify testcase

--- ts-ui-test/src/test/java/TestFlowOne.java
+++ ts-ui-test/src/test/java/TestFlowOne.java
@@ -8,9 +8,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-/**
- * Created by ZDH on 2017/7/19.
- */
+
 public class TestFlowOne {
     @BeforeClass
     public void setUp() throws Exception {
@@ -21,7 +19,6 @@ public class TestFlowOne {
     public void testLogin()throws Exception{
 
         RestTemplate restTemplate = new RestTemplate();
-        //然后向AdminOrderService发送请求，给与权限
         HttpEntity requestEntity = new HttpEntity(null, new HttpHeaders());
         ResponseEntity<Boolean> re = restTemplate.exchange(
                 "http://10.141.212.21:30085/cancelOrder/5ad7750b-a68b-49c0-a8c0-32776b067703",
@@ -30,24 +27,11 @@ public class TestFlowOne {
                 Boolean.class);
 
         if(re.getStatusCodeValue() == 200){
-            //这是抛出OOM的情况
+            throw new Exception("Issue");
         }else{
-            //这是service被kill的情况
+            Assert.assertEquals(1,0);
         }
-
-
-
-
-
-
-
-
     }
-
-
-
-
-
 
     @AfterClass
     public void tearDown() throws Exception {
