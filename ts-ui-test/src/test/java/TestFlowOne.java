f86c137383b524f9e12aceb757c05adb7b2f8df2
Ji Chao <chaojifudan@outlook.com>
2018-04-27 19:37:42 +0800

    modify testcase

--- ts-ui-test/src/test/java/TestFlowOne.java
+++ ts-ui-test/src/test/java/TestFlowOne.java
@@ -8,9 +8,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-/**
- * Created by ZDH on 2017/7/19.
- */
+
 public class TestFlowOne {
     @BeforeClass
     public void setUp() throws Exception {
@@ -22,7 +20,6 @@ public class TestFlowOne {
 
         RestTemplate restTemplate = new RestTemplate();
 
-        //默认不进行自动检查，现在打开自动检查
         HttpEntity requestEntity = new HttpEntity(null, new HttpHeaders());
         ResponseEntity<Boolean> re = restTemplate.exchange(
                 "http://10.141.211.174:30085/cancelOrder/setRecheck/true",
@@ -31,13 +28,10 @@ public class TestFlowOne {
                 requestEntity,
                 Boolean.class);
 
-        //确保请求被执行完毕
         System.out.println("：" + re.getBody().booleanValue());
         Assert.assertEquals(re.getBody().booleanValue(), true);
 
-        //发出十个退票请求，每次间隔十秒
         for (int i = 0; i < 10; i++) {
-            //停顿十秒，给这个辣鸡负载均衡一些反应时间
             Thread.sleep(7000);
             HttpEntity requestEntity2 = new HttpEntity(null, new HttpHeaders());
             ResponseEntity<CancelOrderResult> cancel = restTemplate.exchange(
@@ -46,7 +40,6 @@ public class TestFlowOne {
                     requestEntity2,
                     CancelOrderResult.class);
             System.out.println(cancel.getBody().getMessage());
-            //System.out.println("退订车票：" + cancel.getBody());
             Assert.assertEquals(cancel.getBody().isStatus(), true);
         }
 
@@ -55,7 +48,6 @@ public class TestFlowOne {
 
     @AfterClass
     public void tearDown() throws Exception {
-        //关闭自动检查
         RestTemplate restTemplate = new RestTemplate();
         HttpEntity requestEntity = new HttpEntity(null, new HttpHeaders());
         for (int i = 0; i < 20; i++) {
