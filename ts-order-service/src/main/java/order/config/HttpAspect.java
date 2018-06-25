e0463f39a5a5324e3858de6be3c4f388de60f2ac
lab401\lwh <2391289924@qq.com>
2018-06-25 10:48:38 +0800

    Add log through aop. Change the deployment file to meet the new version of istio

---
+++ ts-order-service/src/main/java/order/config/HttpAspect.java
@@ -0,0 +1,70 @@
+package order.config;
+
+import com.google.gson.Gson;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Aspect
+@Component
+public class HttpAspect {
+
+    private final static org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(HttpAspect.class);
+
+    @Pointcut("execution(public * order.controller.OrderController.*(..))")
+    public void webLog(){}
+
+    @Before("webLog()")
+    public void doBefore(JoinPoint joinPoint){
+
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        String url = request.getRequestURI();
+        String thisServiceName = request.getServerName();
+        String method = request.getMethod();
+        String ip = request.getRemoteAddr();
+        String remoteHost = request.getRemoteHost();
+        String requestArgs  = "";
+        if(joinPoint.getArgs() != null && joinPoint.getArgs().length > 0){
+            for(Object c:  joinPoint.getArgs()) {
+                if( !(c instanceof HttpServletResponse) && !(c instanceof HttpServletRequest)) {
+                    System.out.println("c=" + c);
+                    requestArgs += new Gson().toJson(c);
+                    System.out.println("requestArgs=" + requestArgs);
+                }
+            }
+        }
+
+
+        logger.info("[Service:" + thisServiceName + "]" +
+                    "[URI:" + thisServiceName + url + "]" +
+                    "[Method:" + method + "]" +
+                    "[Request:" + requestArgs + "]" +
+                    "[RemoteHost:" + remoteHost + "]" +
+                    "[IP:" + ip + "]");
+
+    }
+
+    @AfterReturning(returning = "ret", pointcut = "webLog()")
+    public void doAfterReturning(Object ret) throws Throwable {
+
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        String thisServiceName = request.getServerName();
+
+        if(ret != null){
+            logger.info("[Service:" + thisServiceName + "]" + "[Response:" + new Gson().toJson(ret) + "]");
+        }else{
+            logger.info("[Service:" + thisServiceName + "]" + "[Response:void]");
+        }
+    }
+}
