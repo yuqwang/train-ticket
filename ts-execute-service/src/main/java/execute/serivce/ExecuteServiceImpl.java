dc200265e2c64d134ce169ed448bbe64640fa5d1
kylinxiang <15884286060@163.com>
2018-11-28 14:21:56 +0800

    add f1.3 fault

--- ts-execute-service/src/main/java/execute/serivce/ExecuteServiceImpl.java
+++ ts-execute-service/src/main/java/execute/serivce/ExecuteServiceImpl.java
@@ -15,23 +15,23 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 @Service
-public class ExecuteServiceImpl implements ExecuteService {
+public class ExecuteServiceImpl implements ExecuteService{
 
     @Autowired
     private RestTemplate restTemplate;
 
     @Override
-    public TicketExecuteResult ticketExecute(TicketExecuteInfo info, HttpHeaders headers) {
+    public TicketExecuteResult ticketExecute(TicketExecuteInfo info, HttpHeaders headers){
         //1.获取订单信息
         GetOrderByIdInfo getOrderByIdInfo = new GetOrderByIdInfo();
         getOrderByIdInfo.setOrderId(info.getOrderId());
         GetOrderResult resultFromOrder = getOrderByIdFromOrder(getOrderByIdInfo, headers);
         TicketExecuteResult result = new TicketExecuteResult();
         Order order;
-        if (resultFromOrder.isStatus() == true) {
+        if(resultFromOrder.isStatus() == true){
             order = resultFromOrder.getOrder();
             //2.检查订单是否可以进站
-            if (order.getStatus() != OrderStatus.COLLECTED.getCode()) {
+            if(order.getStatus() != OrderStatus.COLLECTED.getCode()){
                 result.setStatus(false);
                 result.setMessage("Order Status Wrong");
                 return result;
@@ -41,54 +41,40 @@ public class ExecuteServiceImpl implements ExecuteService {
             executeInfo.setOrderId(info.getOrderId());
             executeInfo.setStatus(OrderStatus.USED.getCode());
             ModifyOrderStatusResult resultExecute = executeOrder(executeInfo, headers);
-            if (resultExecute.isStatus() == true) {
+            if(resultExecute.isStatus() == true){
                 result.setStatus(true);
                 result.setMessage("Success.");
                 return result;
-            } else {
+            }else{
                 result.setStatus(false);
                 result.setMessage(resultExecute.getMessage());
                 return result;
             }
-        } else {
-            ModifyOrderStatusInfo executeInfo = new ModifyOrderStatusInfo();
-            executeInfo.setOrderId(info.getOrderId());
-            executeInfo.setStatus(OrderStatus.USED.getCode());
-
-            // seq fault
-            List<GetOrderResult> orderResults = new ArrayList<>();
-            List<ModifyOrderStatusResult> orderStatusResults = new ArrayList<>();
-            List<CompletableFuture<Void>> futures = new ArrayList<>();
-
-            getOrderByIdFromOrderOther(getOrderByIdInfo, headers, orderResults, futures);
-            executeOrder(executeInfo, headers, orderResults, orderStatusResults, futures);
-
-            futures.forEach(x -> x.join());
-
-            resultFromOrder = orderResults.get(0);
-            ModifyOrderStatusResult resultExecute = orderStatusResults.get(0);
-
-
-            if (resultFromOrder.isStatus() == true) {
+        }else{
+            resultFromOrder = getOrderByIdFromOrderOther(getOrderByIdInfo, headers);
+            if(resultFromOrder.isStatus() == true){
                 order = resultFromOrder.getOrder();
                 //2.检查订单是否可以进站
-                if (order.getStatus() != OrderStatus.COLLECTED.getCode()) {
+                if(order.getStatus() != OrderStatus.COLLECTED.getCode()){
                     result.setStatus(false);
                     result.setMessage("Order Status Wrong");
                     return result;
                 }
                 //3.确认进站 请求修改订单信息
-
-                if (resultExecute.isStatus() == true) {
+                ModifyOrderStatusInfo executeInfo = new  ModifyOrderStatusInfo();
+                executeInfo.setOrderId(info.getOrderId());
+                executeInfo.setStatus(OrderStatus.USED.getCode());
+                ModifyOrderStatusResult resultExecute = executeOrderOther(executeInfo, headers);
+                if(resultExecute.isStatus() == true){
                     result.setStatus(true);
                     result.setMessage("Success.");
                     return result;
-                } else {
+                }else{
                     result.setStatus(false);
                     result.setMessage(resultExecute.getMessage());
                     return result;
                 }
-            } else {
+            }else{
                 result.setStatus(false);
                 result.setMessage("Order Not Found");
                 return result;
@@ -97,41 +83,56 @@ public class ExecuteServiceImpl implements ExecuteService {
     }
 
     @Override
-    public TicketExecuteResult ticketCollect(TicketExecuteInfo info, HttpHeaders headers) {
+    public TicketExecuteResult ticketCollect(TicketExecuteInfo info, HttpHeaders headers){
         //1.获取订单信息
         GetOrderByIdInfo getOrderByIdInfo = new GetOrderByIdInfo();
         getOrderByIdInfo.setOrderId(info.getOrderId());
-        GetOrderResult resultFromOrder = getOrderByIdFromOrder(getOrderByIdInfo, headers);
+
+        ModifyOrderStatusInfo executeInfo = new ModifyOrderStatusInfo();
+        executeInfo.setOrderId(info.getOrderId());
+        executeInfo.setStatus(OrderStatus.COLLECTED.getCode());
+
+
+        // seq fault
+        List<GetOrderResult> orderResults = new ArrayList<>();
+        List<ModifyOrderStatusResult> orderStatusResults = new ArrayList<>();
+        List<CompletableFuture<Void>> futures = new ArrayList<>();
+
+        getOrderByIdFromOrder(getOrderByIdInfo, headers, orderResults, futures);
+        executeOrder(executeInfo, headers, orderResults, orderStatusResults, futures);
+
+        futures.forEach(x -> x.join());
+
+        GetOrderResult resultFromOrder = orderResults.get(0);
+        ModifyOrderStatusResult resultExecute = orderStatusResults.get(0);
+
+
         TicketExecuteResult result = new TicketExecuteResult();
         Order order;
-        if (resultFromOrder.isStatus() == true) {
+        if(resultFromOrder.isStatus() == true){
             order = resultFromOrder.getOrder();
             //2.检查订单是否可以进站
-            if (order.getStatus() != OrderStatus.PAID.getCode()) {
+            if(order.getStatus() != OrderStatus.PAID.getCode()){
                 result.setStatus(false);
                 result.setMessage("Order Status Wrong");
                 return result;
             }
             //3.确认进站 请求修改订单信息
-            ModifyOrderStatusInfo executeInfo = new ModifyOrderStatusInfo();
-            executeInfo.setOrderId(info.getOrderId());
-            executeInfo.setStatus(OrderStatus.COLLECTED.getCode());
-            ModifyOrderStatusResult resultExecute = executeOrder(executeInfo, headers);
-            if (resultExecute.isStatus() == true) {
+            if(resultExecute.isStatus() == true){
                 result.setStatus(true);
                 result.setMessage("Success.");
                 return result;
-            } else {
+            }else{
                 result.setStatus(false);
                 result.setMessage(resultExecute.getMessage());
                 return result;
             }
-        } else {
+        }else{
             resultFromOrder = getOrderByIdFromOrderOther(getOrderByIdInfo, headers);
-            if (resultFromOrder.isStatus() == true) {
+            if(resultFromOrder.isStatus() == true){
                 order = resultFromOrder.getOrder();
                 //2.检查订单是否可以进站
-                if (order.getStatus() != OrderStatus.PAID.getCode()) {
+                if(order.getStatus() != OrderStatus.PAID.getCode()){
                     result.setStatus(false);
                     result.setMessage("Order Status Wrong");
                     return result;
@@ -141,16 +142,16 @@ public class ExecuteServiceImpl implements ExecuteService {
                 executeInfo.setOrderId(info.getOrderId());
                 executeInfo.setStatus(OrderStatus.COLLECTED.getCode());
                 ModifyOrderStatusResult resultExecute = executeOrderOther(executeInfo, headers);
-                if (resultExecute.isStatus() == true) {
+                if(resultExecute.isStatus() == true){
                     result.setStatus(true);
                     result.setMessage("Success.");
                     return result;
-                } else {
+                }else{
                     result.setStatus(false);
                     result.setMessage(resultExecute.getMessage());
                     return result;
                 }
-            } else {
+            }else{
                 result.setStatus(false);
                 result.setMessage("Order Not Found");
                 return result;
@@ -159,7 +160,7 @@ public class ExecuteServiceImpl implements ExecuteService {
     }
 
 
-    private ModifyOrderStatusResult executeOrder(ModifyOrderStatusInfo info, HttpHeaders headers) {
+    private ModifyOrderStatusResult executeOrder(ModifyOrderStatusInfo info, HttpHeaders headers){
         System.out.println("[Execute Service][Execute Order] Executing....");
         HttpEntity requestEntity = new HttpEntity(info, headers);
         ResponseEntity<ModifyOrderStatusResult> re = restTemplate.exchange(
@@ -174,7 +175,7 @@ public class ExecuteServiceImpl implements ExecuteService {
         return cor;
     }
 
-    private ModifyOrderStatusResult executeOrderOther(ModifyOrderStatusInfo info, HttpHeaders headers) {
+    private ModifyOrderStatusResult executeOrderOther(ModifyOrderStatusInfo info, HttpHeaders headers){
         System.out.println("[Execute Service][Execute Order] Executing....");
         HttpEntity requestEntity = new HttpEntity(info, headers);
         ResponseEntity<ModifyOrderStatusResult> re = restTemplate.exchange(
@@ -189,7 +190,7 @@ public class ExecuteServiceImpl implements ExecuteService {
         return cor;
     }
 
-    private GetOrderResult getOrderByIdFromOrder(GetOrderByIdInfo info, HttpHeaders headers) {
+    private GetOrderResult getOrderByIdFromOrder(GetOrderByIdInfo info, HttpHeaders headers){
         System.out.println("[Execute Service][Get Order] Getting....");
         HttpEntity requestEntity = new HttpEntity(info, headers);
         ResponseEntity<GetOrderResult> re = restTemplate.exchange(
@@ -204,7 +205,7 @@ public class ExecuteServiceImpl implements ExecuteService {
         return cor;
     }
 
-    private GetOrderResult getOrderByIdFromOrderOther(GetOrderByIdInfo info, HttpHeaders headers) {
+    private GetOrderResult getOrderByIdFromOrderOther(GetOrderByIdInfo info, HttpHeaders headers){
         System.out.println("[Execute Service][Get Order] Getting....");
         HttpEntity requestEntity = new HttpEntity(info, headers);
         ResponseEntity<GetOrderResult> re = restTemplate.exchange(
@@ -239,50 +240,6 @@ public class ExecuteServiceImpl implements ExecuteService {
                     ModifyOrderStatusResult.class);
             return re.getBody();
         }).thenAccept(orderStatusResults::add);
-
-        futures.add(future);
-    }
-
-    private void getOrderByIdFromOrder(GetOrderByIdInfo info, HttpHeaders headers, List<GetOrderResult>
-            orderResults, List<CompletableFuture<Void>> futures) {
-        System.out.println("[Execute Service][Get Order] Getting....");
-        HttpEntity<GetOrderByIdInfo> requestEntity = new HttpEntity<>(info, headers);
-
-        CompletableFuture<Void> future = CompletableFuture.supplyAsync(() -> {
-            try {
-                TimeUnit.SECONDS.sleep(4);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            ResponseEntity<GetOrderResult> re = restTemplate.exchange(
-                    "http://ts-order-service:12031/order/getById/",
-                    HttpMethod.POST,
-                    requestEntity,
-                    GetOrderResult.class);
-            return re.getBody();
-        }).thenAccept(orderResults::add);
-
-        futures.add(future);
-    }
-
-    private void getOrderByIdFromOrderOther(GetOrderByIdInfo info, HttpHeaders headers, List<GetOrderResult>
-            orderResults, List<CompletableFuture<Void>> futures) {
-        System.out.println("[Execute Service][Get Order] Getting....");
-        HttpEntity<GetOrderByIdInfo> requestEntity = new HttpEntity<>(info, headers);
-
-        CompletableFuture<Void> future = CompletableFuture.supplyAsync(() -> {
-            try {
-                TimeUnit.SECONDS.sleep(4);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            ResponseEntity<GetOrderResult> re = restTemplate.exchange(
-                    "http://ts-order-other-service:12032/orderOther/getById/",
-                    HttpMethod.POST,
-                    requestEntity,
-                    GetOrderResult.class);
-            return re.getBody();
-        }).thenAccept(orderResults::add);
 
         futures.add(future);
     }
