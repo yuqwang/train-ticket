ca10b9cd1e0dbb65d54d61a2beea4b75c586fbd0
hechuan73 <1044856164@qq.com>
2019-11-12 15:55:19 +0800

    Description: change method name.

--- ts-auth-service/src/main/java/auth/repository/UserRepository.java
+++ ts-auth-service/src/main/java/auth/repository/UserRepository.java
@@ -3,10 +3,12 @@ package auth.repository;
 import auth.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
     Optional<User> findByUsername(String username);
@@ -19,5 +21,5 @@ public interface UserRepository extends MongoRepository<User, String> {
      * @return the target user if it exists, if not, return null.
      */
     @Query("{ 'username': ?0 }")
-    Optional<User> get(String username);
+    Optional<User> getUser(String username);
 }
