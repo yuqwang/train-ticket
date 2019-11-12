a94a6f19ee45427ac3c38a0915dae77572e74720
hechuan73 <1044856164@qq.com>
2019-11-12 14:36:58 +0800

    Description: add methods for fault injection.

--- ts-auth-service/src/main/java/auth/repository/UserRepository.java
+++ ts-auth-service/src/main/java/auth/repository/UserRepository.java
@@ -2,6 +2,7 @@ package auth.repository;
 
 import auth.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -11,4 +12,12 @@ public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByUsername(String username);
 
     void deleteByUserId(UUID userId);
+
+    /**
+     * Mock for fault injection: get user by username
+     * @param username username
+     * @return the target user if it exists, if not, return null.
+     */
+    @Query("{ 'username': ?0 }")
+    Optional<User> get(String username);
 }
