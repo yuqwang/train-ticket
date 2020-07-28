b269ff8e9545ea53c5b606a07aeecd217cd98cb7
hechuan73 <1044856164@qq.com>
2020-07-28 16:12:03 +0800

    Description: remove unused imports.

--- ts-user-service/src/main/java/user/UserApplication.java
+++ ts-user-service/src/main/java/user/UserApplication.java
@@ -2,7 +2,7 @@ package user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * @author fdse
