00fed1e21ccf51d486d2aba4cb671230ead827d4
LeBW <27091925@qq.com>
2021-07-13 14:59:36 +0800

    modify ts-cancel-service error: delay cancel order.

--- ts-cancel-service/src/main/java/cancel/async/AsyncTask.java
+++ ts-cancel-service/src/main/java/cancel/async/AsyncTask.java
@@ -32,7 +32,15 @@ public class AsyncTask {
     private static final Logger LOGGER = LoggerFactory.getLogger(AsyncTask.class);
 
     @Async("myAsync")
-    public Future<Response> cancelFromOrder(Order order, HttpHeaders headers) throws InterruptedException{
+    public Future<Response> cancelFromOrder(Order order, HttpHeaders headers) throws InterruptedException {
+        /*********************** Fault Reproduction - Error Process Seq *************************/
+        double op = new Random().nextDouble();
+        if (op < 1.0) {
+            AsyncTask.LOGGER.info("[Cancel Order Service] Delay Process，Wrong Cancel Process");
+            Thread.sleep(4000);
+        } else {
+            AsyncTask.LOGGER.info("[Cancel Order Service] Normal Process，Normal Cancel Process");
+        }
 
         AsyncTask.LOGGER.info("[Change Order Status] Changing...");
         order.setStatus(OrderStatus.CANCEL.getCode());
@@ -51,23 +59,16 @@ public class AsyncTask {
     }
 
     @Async("myAsync")
-    public Future<Boolean> drawBackMoney(String money, String userId, HttpHeaders headers) throws InterruptedException{
+    public Future<Boolean> drawBackMoney(String money, String userId, HttpHeaders headers) throws InterruptedException {
 
-        /*********************** Fault Reproduction - Error Process Seq *************************/
-        double op = new Random().nextDouble();
-        if(op < 1.0){
-            AsyncTask.LOGGER.info("[Cancel Order Service] Delay Process，Wrong Cancel Process");
-            Thread.sleep(4000);
-        } else {
-            AsyncTask.LOGGER.info("[Cancel Order Service] Normal Process，Normal Cancel Process");
-        }
 
         AsyncTask.LOGGER.info("[Draw Back Money] Draw back money...");
 
         HttpHeaders newHeaders = getAuthorizationHeadersFrom(headers);
         HttpEntity requestEntity = new HttpEntity(newHeaders);
         ResponseEntity<Response> re = restTemplate.exchange(
-                "http://ts-inside-payment-service:18673/api/v1/inside_pay_service/inside_payment/drawback/" + userId + "/" + money,
+                "http://ts-inside-payment-service:18673/api/v1/inside_pay_service/inside_payment/drawback/" + userId
+                        + "/" + money,
                 HttpMethod.GET,
                 requestEntity,
                 Response.class);
