e4a57bd5eaca0f962a00ab4c0f1cec66a03b0a06
Humbert Zhang <humbertzk@gmail.com>
2021-07-22 15:16:11 +0800

    Add async call using rabbitmq.  (#1)
    Modify ts-food-service , send food order info by mq to delivery service now.
    Add ts-delivery-service.
    Modify preserve and preserve-other service's send mail part into async
    Modify ts-notification-service, it pull emails from mq and then send it
    Modify K8s deploy files
    
    (cherry picked from commit 28baa9a84dcea184a5a2a7644e7e8b7a3c2e6978)

--- ts-preserve-other-service/src/main/java/preserveOther/service/PreserveOtherServiceImpl.java
+++ ts-preserve-other-service/src/main/java/preserveOther/service/PreserveOtherServiceImpl.java
@@ -1,5 +1,6 @@
 package preserveOther.service;
 
+import edu.fudan.common.util.JsonUtils;
 import edu.fudan.common.util.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import preserveOther.entity.*;
+import preserveOther.mq.RabbitSend;
 
 import java.util.Date;
 import java.util.UUID;
@@ -24,6 +26,9 @@ public class PreserveOtherServiceImpl implements PreserveOtherService {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private RabbitSend sendService;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PreserveOtherServiceImpl.class);
 
@@ -260,16 +265,17 @@ public class PreserveOtherServiceImpl implements PreserveOtherService {
     }
 
     public boolean sendEmail(NotifyInfo notifyInfo, HttpHeaders httpHeaders) {
+        PreserveOtherServiceImpl.LOGGER.info("[Preserve Other Service][Send Email] send email to mq");
 
-        PreserveOtherServiceImpl.LOGGER.info("[Preserve Service][Send Email]");
-        HttpEntity requestEntitySendEmail = new HttpEntity(notifyInfo, httpHeaders);
-        ResponseEntity<Boolean> reSendEmail = restTemplate.exchange(
-                "http://ts-notification-service:17853/api/v1/notifyservice/notification/preserve_success",
-                HttpMethod.POST,
-                requestEntitySendEmail,
-                Boolean.class);
+        try {
+            String infoJson = JsonUtils.object2Json(notifyInfo);
+            sendService.send(infoJson);
+        } catch (Exception e) {
+            PreserveOtherServiceImpl.LOGGER.error("[Preserve Other Service] send email to mq error, exception is:" + e);
+            return false;
+        }
 
-        return reSendEmail.getBody();
+        return true;
     }
 
     public User getAccount(String accountId, HttpHeaders httpHeaders) {
