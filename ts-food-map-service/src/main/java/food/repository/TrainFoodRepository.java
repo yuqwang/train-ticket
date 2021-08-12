925af27d40c77d558c89ba0c15be90aac964d67c
zhongyuanzhao000 <zhongyuanzhao000@gmail.com>
2021-08-12 16:01:05 +0800

    ts-food-map-service: migrate from mongo to mysql

--- ts-food-map-service/src/main/java/food/repository/TrainFoodRepository.java
+++ ts-food-map-service/src/main/java/food/repository/TrainFoodRepository.java
@@ -2,22 +2,20 @@ package food.repository;
 
 
 import food.entity.TrainFood;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface TrainFoodRepository extends MongoRepository<TrainFood, String> {
+public interface TrainFoodRepository extends CrudRepository<TrainFood, String> {
 
     TrainFood findById(UUID id);
 
     @Override
     List<TrainFood> findAll();
 
-    @Query("{ 'tripId' : ?0 }")
     List<TrainFood> findByTripId(String tripId);
 
     void deleteById(UUID id);
