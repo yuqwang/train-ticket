a9bfc087c55656f3d16cea13a740b5fe2a84ebba
程千骞 <1617095973@qq.com>
2022-08-11 15:48:32 +0800

    swagger2

--- ts-wait-order-service/src/main/java/waitorder/controller/WaitListOrderController.java
+++ ts-wait-order-service/src/main/java/waitorder/controller/WaitListOrderController.java
@@ -1,12 +1,18 @@
 package waitorder.controller;
 
 
+import edu.fudan.common.entity.Contacts;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
+import waitorder.entity.WaitListOrder;
 import waitorder.entity.WaitListOrderVO;
 import waitorder.service.WaitListOrderService;
 
@@ -31,18 +37,31 @@ public class WaitListOrderController {
     }
 
     @PostMapping(path = "/order")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "createOrder",value = "createOrder",dataType = "WaitListOrderVO", paramType = "body",required = true)
+    })
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Thread Start Success"),
+            @ApiResponse(code = 400, message = "cache to database failed", response = WaitListOrder.class,responseContainer = "Response")
+    })
     public HttpEntity createNewOrder(@RequestBody WaitListOrderVO createOrder, @RequestHeader HttpHeaders headers) {
         WaitListOrderController.LOGGER.info("[createWaitOrder][Create Wait Order][from {} to {} at {}]", createOrder.getFrom(), createOrder.getTo(), createOrder.getDate());
         return ok(waitListOrderService.create(createOrder, headers));
     }
 
     @GetMapping(path = "/orders")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Success", response = WaitListOrder.class,responseContainer = "List")
+    })
     public HttpEntity getAllOrders(@RequestHeader HttpHeaders headers){
         LOGGER.info("[getAllOrders][Get All Orders]");
         return ok(waitListOrderService.getAllOrders(headers));
     }
 
     @GetMapping(path = "/waitlistorders")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Success", response = WaitListOrder.class,responseContainer = "List")
+    })
     public HttpEntity getWaitListOrders(@RequestHeader HttpHeaders headers){
         LOGGER.info("[getWaitListOrders][Get All Wait List Orders]");
         return ok(waitListOrderService.getAllWaitListOrders(headers));
