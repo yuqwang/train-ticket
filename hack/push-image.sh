e4df4411973ff0222a4f610ef8a1a08e675831da
Kyle-Dwyer <1005284123@qq.com>
2022-08-15 16:53:06 +0800

    yaml

--- hack/push-image.sh
+++ hack/push-image.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -eu
 
-echo
-echo "Please input you repo password of $1"
-echo
-docker login --username="$1"
+#echo
+#echo "Please input you repo password of $1"
+#echo
+#docker login --username="$1"
 
 echo
 echo "Start pushing image"
