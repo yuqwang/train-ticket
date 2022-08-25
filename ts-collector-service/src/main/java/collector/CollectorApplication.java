14e08f2093b8edf8abd1053a8bd896809cd243b2
Kyle-Dwyer <1005284123@qq.com>
2022-08-25 10:18:59 +0800

    test

--- ts-collector-service/src/main/java/collector/CollectorApplication.java
+++ ts-collector-service/src/main/java/collector/CollectorApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -14,6 +17,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * @author humbertzhang
  */
 @SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableAsync
+@IntegrationComponentScan
 @EnableDiscoveryClient
 public class CollectorApplication {
 
--- ts-collector-service/src/main/java/collector/mq/CoverageRabbitReceive.java
+++ ts-collector-service/src/main/java/collector/mq/CoverageRabbitReceive.java
@@ -5,9 +5,7 @@ import collector.config.CoverageQueues;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 
 
@@ -15,9 +13,6 @@ import org.springframework.web.client.RestTemplate;
 public class CoverageRabbitReceive {
 
     private static final Logger logger = LoggerFactory.getLogger(CoverageRabbitReceive.class);
-
-    @Autowired
-    private RestTemplate restTemplate;
 
     @RabbitListener(queues = CoverageQueues.queueName)
     public void process(String payload) {
