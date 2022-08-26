2b43396362c96f11fcbaf7e2f0e6db459d4c26ff
Kyle-Dwyer <1005284123@qq.com>
2022-08-26 10:23:03 +0800

    filter

--- ts-consign-price-service/src/main/java/consignprice/ConsignPriceApplication.java
+++ ts-consign-price-service/src/main/java/consignprice/ConsignPriceApplication.java
@@ -3,7 +3,7 @@ package consignprice;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
